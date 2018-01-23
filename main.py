a68d17c67c094051f28370ae78efdb60aff60a05
funs <pc904b28@yahoo.com.tw>
2018-01-24 03:19:15 +0800

    copy code from kivyexample/guide/designwithkv

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 # -*-coding=UTF-8-*-
 
-import os
-import kivy.app
+import kivy
+kivy.require('1.10.0')
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
+
+
+class SpeduWidget(FloatLayout):
+
+    label_wid = ObjectProperty()
+    info = StringProperty()
+
+    def do_action(self):
+        self.label_wid.text = 'My label after button press'
+        self.info = 'New info text'
+
+
+class SpeduApp(App):
+
+    def build(self):
+        return SpeduWidget(info='Hello world')
+
+
+if __name__ == '__main__':
+    SpeduApp().run()
